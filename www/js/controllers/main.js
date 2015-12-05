928f8f66548122987889e3a6cd7b382745c66a79
Fidh <nexdigital.software@gmail.com>
2015-12-05 18:44:10 +0700

    update

--- www/js/controllers/main.js
+++ www/js/controllers/main.js
@@ -1,4 +1,18 @@
-var mainController = angular.module('mainController',[]);
+var mainController = angular.module('mainController',['ngCordova']);
+
+mainController.controller('main.controller',function($rootScope,$cordovaBarcodeScanner){
+	$rootScope.addItem = function(){
+		document.addEventListener("deviceready", function () {
+			$cordovaBarcodeScanner
+			.scan()
+			.then(function(barcodeData) {
+				alert(barcodeData);
+			}, function(error) {
+				alert(error);
+			});
+		});	
+	}
+});
 
 mainController.controller('dashboard.main',function($scope){
 	$scope.pageTitle = "Tata Courier";
